710e7eb65ef230b0ce5520769b3d56f62087bde2
tkoar <terrancekoar@gmail.com>
2017-05-04 17:29:28 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+    name = []
+    name << self.first_name
+    name << self.last_name
+    name.join(" ")
+  end
+
+  def list_roles
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,9 @@
+require 'pry'
 class Network < ActiveRecord::Base
   has_many :shows
 
   def sorry
+    binding.pry
     "We're sorry about passing on John Mullaney's pilot"
   end
 end
